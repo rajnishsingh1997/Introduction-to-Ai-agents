10ff6e7b54f272334081c7f0888d090baf54c530
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-12-29 17:15:10 +0530

    changes for tools,system prompt

--- Backend/controller/chat.controller.js
+++ Backend/controller/chat.controller.js
@@ -3,7 +3,7 @@ import weatherAgent from "../services/agent.js";
 const chatController = (req, res, next) => {
   const { message } = req.body;
   const response = weatherAgent("hello");
-  console.log(response);
+ 
   res.status(200).json({
     success: true,
     message: response,
