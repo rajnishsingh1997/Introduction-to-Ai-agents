2016e52fc92d1097375026723bef79a22c493333
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-12-29 16:12:20 +0530

    controller code cleanup

--- Backend/controller/chat.controller.js
+++ Backend/controller/chat.controller.js
@@ -1,10 +1,5 @@
 const chatController = (req, res, next) => {
   const { message } = req.body;
-  console.log(message);
-  res.status(200).json({
-    success: true,
-    message: "reached controller code",
-  });
 };
 
 export default chatController;
