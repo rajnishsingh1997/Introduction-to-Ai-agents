0720be69e9bb125a25be5081775fc3c92a77f0bd
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-12-31 10:26:09 +0530

    added changes

--- Backend/controller/chat.controller.js
+++ Backend/controller/chat.controller.js
@@ -1,5 +1,3 @@
-import weatherAgent from "../services/agent.js";
-import getWeatherDetails from "../tool/getWeatherTool.js";
 
 const chatController = (req, res, next) => {
   const { message } = req.body;
--- Backend/tool/getWeatherTool.js
+++ Backend/tool/getWeatherTool.js
@@ -1,11 +1,13 @@
-import { response } from "express";
-
 const getWeatherDetails = async (cityName) => {
-  const response =
-    await fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${cityName}&aqi=yes
+  try {
+    const response =
+      await fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${cityName}&aqi=yes
 `);
-  // return the whole response,might be useful
-  return response;
+
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default getWeatherDetails;
